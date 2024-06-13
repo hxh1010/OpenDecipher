9500556de4c88b6758c4a63a7dbecefbec141a7f
Xinhang Hou <hxh1010@users.noreply.github.com>
2024-06-14 00:05:23 +0800

    Update model_Transformer.py

--- model_Transformer.py
+++ model_Transformer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics import confusion_matrix
 import math 
-
+ 
 seed = 5
 torch.manual_seed(seed)  # 为CPU设置随机种子
 torch.cuda.manual_seed(seed)  # 为当前GPU设置随机种子
